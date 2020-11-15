479d1e0c1622e47e3f2442fb794da99d7ce2141a
Tobias Glock <tobias.glock@msg.group>
2020-11-15 20:49:47 +0100

    Cleaned up api

--- images/api/api.py
+++ images/api/api.py
@@ -1,14 +1,8 @@
 from typing import Optional
 from fastapi import FastAPI
-from waitress import serve
 
 app = FastAPI()
 
 @app.get("/")
 async def root(name: Optional[str] = "World"):
     return {'greeting': 'Hello', 'name': name}
-
-#Create the main driver function
-if __name__ == '__main__':
-    #call the run method
-    serve(app, host="0.0.0.0", port=8081)
